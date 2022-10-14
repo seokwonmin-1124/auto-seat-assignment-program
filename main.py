d654e77e204576295316cac45505ddfee73f5fd1
seokwonmin-1124 <seokwonmin@naver.com>
2022-10-15 00:49:00 +0900

    오류 수정

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ print(네이밍뭐로하지(30, 5))
 
 # def 풀어서적은코드(stdNum, n):
 #     stdList = []
+#     _stdList = []
 
 #     for i in range(stdNum):
-#         stdList.append(i+1)
-#     random.shuffle(stdList)
+#         _stdList.append(i+1)
+#     random.shuffle(_stdList)
 
-#     for i in range((len(stdList) + n - 1) // n ):
-#         stdList.append(stdList[i * n:(i + 1) * n])
+#     for i in range((len(_stdList) + n - 1) // n ):
+#         stdList.append(_stdList[i * n:(i + 1) * n])
 #     return stdList
 
 # print(풀어서적은코드(30, 5))
