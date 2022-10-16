34c21caa8baef68779cded3e82fe8a7ecdb47a73
seokwonmin-1124 <seokwonmin@naver.com>
2022-10-17 01:22:23 +0900

    코드정리

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import random
 import numpy as np
-import streamlit as st
 
 stdNum = 30
 
-def 풀어서적은코드(stdNum, n): # n = 줄 몇개로 쪼갤지
+def createNewPlacement(stdNum, n): # n = 줄 몇개로 쪼갤지
     stdList = []
     stdNumList = []
 
@@ -14,8 +13,5 @@ def 풀어서적은코드(stdNum, n): # n = 줄 몇개로 쪼갤지
 
     for i in range((len(stdNumList) + n - 1) // n ):
         stdList.append(stdNumList[i * n:(i + 1) * n])
-
+        
     return stdList
-
-st.write('### 교탁')
-st.write(np.asarray(풀어서적은코드(stdNum, 5)))
